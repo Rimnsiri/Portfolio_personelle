a08922801771e66a43ed88396bb191cd201d40d8
nsiririm <nsiririm111@gmail.com>
2024-12-08 15:10:34 +0100

    first_uplode

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Full-stack Developer",
+          "Freelancer",
+          "Frontend Developer",
+          "Backend Developer",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
